88fb9c1c684cfc1f0e76670b5b9fd5e5cdd2f520
kazimahin68 <kazimahin68@gmail.com>
2023-05-14 03:13:18 +0600

    delete operation

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 console.log()
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j52gohc.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -33,6 +33,21 @@ async function run() {
             const newCoffee = req.body;
             // console.log(newCoffee)
             const result = await coffeeCollection.insertOne(newCoffee);
+            res.send(result)
+        })
+
+        // Get Data from the server site
+        app.get('/coffees', async(req, res)=>{
+            const cursor = coffeeCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        // Delete a Coffee
+        app.delete('/coffees/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id : new ObjectId(id)}
+            const result = await coffeeCollection.deleteOne(query);
             res.send(result)
         })
 
